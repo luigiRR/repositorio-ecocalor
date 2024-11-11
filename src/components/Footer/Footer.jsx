6d5cb2aa8dbed43ebe112c4a0110d95855de682b
luigiRR <luigi.rocha@unas.edu.pe>
2024-11-11 14:23:46 -0500

    update to responsive

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,87 +1,71 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFacebook, faInstagram, faTwitter, faYoutube, faTiktok } from '@fortawesome/free-brands-svg-icons'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faInstagram, faTwitter, faYoutube, faTiktok } from '@fortawesome/free-brands-svg-icons';
 
 export default function Footer() {
     return (
         <div className="bg-cover bg-center">
             <div className="bg-gray-700">
-                <div className="container mx-auto px-6 lg:px-20 py-4">
-                    <div className="lg:flex">
-                        <div className="w-full lg:w-2/3">
-                            <div className="lg:flex">
-                                <div className="w-full mb-12 lg:mb-0 lg:w-1/2">
+                <div className="container mx-auto px-4 sm:px-6 lg:px-20 py-6">
+                    <div className="flex flex-col lg:flex-row">
+                        <div className="w-full lg:w-2/3 mb-6 lg:mb-0">
+                            <div className="flex flex-col lg:flex-row">
+                                <div className="w-full lg:w-1/2 mb-6 lg:mb-0">
                                     <h2 className="font-bold text-xl text-gray-50 mb-4">SOL DE MIMMI</h2>
                                     <p className="text-gray-50 text-justify">
-                                        SOL DE MIMMI is a site for developer, student and learner,
-                                        where they can grab
-                                        hundreds of various html component code and try online as fast as posible without...
+                                        SOL DE MIMMI es un sitio para desarrolladores, estudiantes y aprendices,
+                                        donde pueden obtener cientos de componentes de HTML y probar código en línea rápidamente.
                                     </p>
-                                    <div className="flex mt-6">
-                                        <i className="flex items-center justify-center h-12 w-12 mr-1 rounded-full fab fill-current text-white text-xl fa-facebook-f"></i>
-                                        <i lassName="flex items-center justify-center h-12 w-12 mx-1 rounded-full fas fill-current text-white text-xl fa-envelope"></i>
-                                        <i className="flex items-center justify-center h-12 w-12 mx-1 rounded-full fab fill-current text-white text-xl fa-instagram"></i>
-                                        <i className="flex items-center justify-center h-12 w-12 mx-1 rounded-full fab fill-current text-white text-xl fa-twitter"></i>
+                                    <div className="flex mt-6 space-x-2">
+                                        <FontAwesomeIcon icon={faFacebook} className="text-white text-2xl" />
+                                        <FontAwesomeIcon icon={faInstagram} className="text-white text-2xl" />
+                                        <FontAwesomeIcon icon={faTwitter} className="text-white text-2xl" />
+                                        <FontAwesomeIcon icon={faYoutube} className="text-white text-2xl" />
                                     </div>
                                 </div>
-                                <div className="w-full lg:w-1/2 lg:flex lg:px-6 ">
-                                    <div className="w-full mb-6 lg:mb-0 lg:w-1/2">
-                                        <h2 className="font-bold text-gray-50 mb-4 text-xl">
-                                            Menú</h2>
+                                <div className="w-full lg:w-1/2 flex flex-col lg:flex-row lg:space-x-6">
+                                    <div className="mb-6 lg:mb-0 w-full lg:w-1/2">
+                                        <h2 className="font-bold text-gray-50 mb-4 text-xl">Menú</h2>
                                         <ul className="text-gray-50 text-sm">
-                                            <li className="pt-1 pb-2">
-                                                <Link to="/">Inicio</Link>
-                                            </li>
-                                            <li className="pt-1 pb-2">
-                                                <Link to="/Doc">Documentos</Link>
-                                            </li>
-                                            <li className="pt-1 pb-2">
-                                                <Link to="/About">Nosotros</Link>
-                                            </li>
-                                            <li className="pt-1 pb-2">
-                                                <Link to="/Services">Servicios</Link>
-                                            </li>
-                                            {/* <li className="pt-1 pb-2">
-                                                <Link to="/Blog">Blog</Link>
-                                            </li> */}
-                                            <li className="pt-1 pb-2">
-                                                <Link to="/Contact">Contactanos</Link>
-                                            </li>
+                                            <li className="pt-1 pb-2"><Link to="/">Inicio</Link></li>
+                                            <li className="pt-1 pb-2"><Link to="/Doc">Documentos</Link></li>
+                                            <li className="pt-1 pb-2"><Link to="/About">Nosotros</Link></li>
+                                            <li className="pt-1 pb-2"><Link to="/Services">Servicios</Link></li>
+                                            <li className="pt-1 pb-2"><Link to="/Contact">Contactanos</Link></li>
                                         </ul>
                                     </div>
-                                    <div className="w-full mb-6 lg:mb-0 lg:w-1/2">
-                                        <h2 className="font-bold text-gray-50 mb-4 text-xl">
-                                            Redes Sociales</h2>
+                                    <div className="w-full lg:w-1/2">
+                                        <h2 className="font-bold text-gray-50 mb-4 text-xl">Redes Sociales</h2>
                                         <ul className="text-gray-50 text-sm">
                                             <li className="pt-1 pb-2">
-                                                <a href="https://www.facebook.com/Ecocalor-Calefacci%C3%B3n-Y-Ventilaci%C3%B3n-284982982430389/?ref=pages_you_manage" className="flex flex-wrap hover:text-blue-700">
-                                                    <div className="pt-1 pr-3"><p>Facebook</p></div>
-                                                    <div><FontAwesomeIcon icon={faFacebook} size="2x"/></div>
+                                                <a href="https://www.facebook.com/EcocalorCV" className="flex items-center hover:text-blue-700">
+                                                    <span className="mr-2">Facebook</span>
+                                                    <FontAwesomeIcon icon={faFacebook} size="lg" />
                                                 </a>
                                             </li>
                                             <li className="pt-1 pb-2">
-                                                <a href="https://www.instagram.com/ecocalorcv/" className="flex flex-wrap hover:text-red-400">
-                                                    <div className="pt-1 pr-2.5"><p>Instagram</p></div>
-                                                    <div><FontAwesomeIcon icon={faInstagram} size="2x"/></div>
+                                                <a href="https://www.instagram.com/ecocalorcv/" className="flex items-center hover:text-red-400">
+                                                    <span className="mr-2">Instagram</span>
+                                                    <FontAwesomeIcon icon={faInstagram} size="lg" />
                                                 </a>
                                             </li>
                                             <li className="pt-1 pb-2">
-                                                <a href="https://twitter.com/EcocalorCV" className="flex flex-wrap hover:text-blue-400">
-                                                    <div className="pt-1 pr-8"><p>Twitter</p></div>
-                                                    <div><FontAwesomeIcon icon={faTwitter} size="2x"/></div>
+                                                <a href="https://twitter.com/EcocalorCV" className="flex items-center hover:text-blue-400">
+                                                    <span className="mr-2">Twitter</span>
+                                                    <FontAwesomeIcon icon={faTwitter} size="lg" />
                                                 </a>
                                             </li>
                                             <li className="pt-1 pb-2">
-                                                <a href="https://www.youtube.com/channel/UCKy5yVeXVXekNkyq5c2jtwg" className="flex flex-wrap hover:text-red-600">
-                                                    <div className="pt-1 pr-3.5"><p>YouTube</p></div>
-                                                    <div><FontAwesomeIcon icon={faYoutube} size="2x"/></div>
+                                                <a href="https://www.youtube.com/channel/UCKy5yVeXVXekNkyq5c2jtwg" className="flex items-center hover:text-red-600">
+                                                    <span className="mr-2">YouTube</span>
+                                                    <FontAwesomeIcon icon={faYoutube} size="lg" />
                                                 </a>
                                             </li>
                                             <li className="pt-1 pb-2">
-                                                <a href="" className="flex flex-wrap hover:text-pink-500">
-                                                    <div className="pt-1 pr-8"><p>TikTok</p></div>
-                                                    <div><FontAwesomeIcon icon={faTiktok} size="2x"/></div>
+                                                <a href="" className="flex items-center hover:text-pink-500">
+                                                    <span className="mr-2">TikTok</span>
+                                                    <FontAwesomeIcon icon={faTiktok} size="lg" />
                                                 </a>
                                             </li>
                                         </ul>
@@ -89,24 +73,21 @@ export default function Footer() {
                                 </div>
                             </div>
                         </div>
-                        <div className="w-full lg:w-1/3">
-                            <div className="text-justify text-gray-50 mb-1">
-                                <p>Si envias un e-mail con tus datos personales, que voluntariamente
-                                    decides comunicarnos, estos serán utilizados unicamente para responder
-                                    tu mensaje.
-                                    No cumincaremos tús datos a terceros
-                                </p>
-                            </div>
-                            <div className="justify-center text-gray-50 mb-1 mt-6">
-                                © 2021  <span className="font-bold">SOL DE MIMMI</span> 2021 todos los derechos reservados.
-                            </div>
-                            <div className="font-light justify-center text-gray-50 text-sm">
-                                <p>Designed by <span className="font-bold">SOL DE MIMMI</span></p>
-                            </div>
+                        <div className="w-full lg:w-1/3 text-gray-50 text-sm">
+                            <p className="text-justify mb-6">
+                                Si envías un e-mail con tus datos personales, estos serán utilizados únicamente para responder
+                                tu mensaje. No compartiremos tus datos con terceros.
+                            </p>
+                            <p className="text-center mb-6">
+                                © 2021 <span className="font-bold">SOL DE MIMMI</span> - Todos los derechos reservados.
+                            </p>
+                            <p className="text-center">
+                                Diseñado por <span className="font-bold">SOL DE MIMMI</span>
+                            </p>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    )
+    );
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,25 +1,27 @@
-import React from 'react'
-import Navigation from '../../components/Navigation/Navigation'
-import Slider from '../../components/Hero-Slider/Slider'
-import TitleHead from '../../components/UI/TitleHead/TitleHead'
-import Modals from '../../components/UI/Modal/Modals'
-import Footer from '../../components/Footer/Footer'
+import React from 'react';
+import Navigation from '../../components/Navigation/Navigation';
+import Slider from '../../components/Hero-Slider/Slider';
+import TitleHead from '../../components/UI/TitleHead/TitleHead';
+import Modals from '../../components/UI/Modal/Modals';
+import Footer from '../../components/Footer/Footer';
 
 export default function Home() {
-
     return (
         <div className="">
-            <Navigation/>
-            <Slider/>
-            <TitleHead>desarrollamos sistemas orientados a la eficiencia de la energia termica</TitleHead>
-            <div className="container grid grid-cols-2 gap-4 mb-3">
-                <div className="">
-                    <div>
-                        <br />
-                        <p className="pb-2 text-red-700 text-2xl">
+            <Navigation />
+            <Slider />
+            <TitleHead>
+                desarrollamos sistemas orientados a la eficiencia de la energia termica
+            </TitleHead>
+            
+            {/* Ajustes de la cuadrícula para diferentes tamaños de pantalla */}
+            <div className="container grid grid-cols-1 lg:grid-cols-2 gap-4 mb-3">
+                <div>
+                    <div className="p-4 lg:p-0">
+                        <p className="pb-2 text-red-700 text-xl lg:text-2xl">
                             ¿Cuanta energía termica necesita una edificación?
-                        </p> 
-                        <p className="text-justify">
+                        </p>
+                        <p className="text-justify text-sm lg:text-base">
                             Los principales problemas que por conservación y convención se
                             ha venido desarrollando en los sistemas de calefacción y
                             ventilación, es el NO estudio del diseño, la instalación y
@@ -31,17 +33,19 @@ export default function Home() {
                         </p>
                     </div>
                 </div>
-                <div>
-                    <div className="mt-5 ml-10">
-                        <img className='object-cover rounded-lg shadow-md' alt="decorative" src="https://firebasestorage.googleapis.com/v0/b/supporttickets-cb452.appspot.com/o/ecocalor%2Fmiraflores.jpg?alt=media&token=8124b70d-87cd-44de-ace9-851fa1a7fea1"></img>
-                    </div>
+
+                {/* Ajuste de imagen responsivo */}
+                <div className="flex justify-center mt-5 lg:mt-0">
+                    <img
+                        className="object-cover rounded-lg shadow-md w-full sm:w-3/4 md:w-2/3 lg:w-auto"
+                        alt="decorative"
+                        src="https://firebasestorage.googleapis.com/v0/b/supporttickets-cb452.appspot.com/o/ecocalor%2Fmiraflores.jpg?alt=media&token=8124b70d-87cd-44de-ace9-851fa1a7fea1"
+                    />
                 </div>
 
-                <div className="">
-                    <span></span>
-                    <h2 className="uppercase text-red-700 text-2xl">el comfort termico</h2>
-                    <div>
-                    <p className="text-justify pb-2">
+                <div>
+                    <h2 className="uppercase text-red-700 text-xl lg:text-2xl">el comfort termico</h2>
+                    <p className="text-justify pb-2 text-sm lg:text-base">
                         Representa la opción del organismo humano de cambiar con el ambiente la
                         cantidad de calor necesaria para la función metabólica, a través
                         de una equilibrada relación entre los procesos de radiación,
@@ -49,47 +53,50 @@ export default function Home() {
                         que determinan el comfort térmico son muchos, pero los más
                         importantes son:
                     </p>
-                    <Modals/>
-                    </div>
-                </div>
-                <div className="text-center">
-                    <div className="mt-5 ml-10">
-                        <img className='object-cover rounded-lg shadow-md' alt="decorative" src='https://firebasestorage.googleapis.com/v0/b/supporttickets-cb452.appspot.com/o/ecocalor%2Fse%C3%B1ora.jpg?alt=media&token=75329952-13ec-4aaf-ad28-5cf896b604a3' />
-                    </div>
+                    <Modals />
                 </div>
 
-                <div className="">
-                    <span></span>
-                    <h2 className="uppercase text-red-700 text-2xl">instalación térmica</h2>
-                    <div>
-                        <p className="text-justify">
-                            La moderna tecnología del sistema de instalación térmica,
-                            permite obtener una calefacción en los ambientes de la edificación,
-                            utilizando la radiación del sol, que por medio de la emisión, propagación y
-                            transferencia de energia en forma de ondas electromagnéticas o partículas, que proporciona calor;
-                            el cual viene transferida al fluido vector, esta energia incrementa el calor
-                            del fluido al interno de las tuberias.
-                            El incremento de temperatura del fluido vector es transmitido a los
-                            ambientes requeridos por medio de los escambiadores de calor,
-                            utilizando la convección y radiación para transferir calor.
-                            Nuestro sistema de instalación térmica, se integra
-                            perfectamente con las envolventes de la edificación; otorgando
-                            como resultado el comfort térmico deseado, con temperaturas comprendidas 
-                            entre 20°C y 25°C con una humedad relativa entre 40% y 60%; siendo estos los
-                            parametros para un correcto ambiente comfortable, según el organismo internacional ASHRAE.
-                        </p>
-                    </div>
+                <div className="flex justify-center mt-5 lg:mt-0">
+                    <img
+                        className="object-cover rounded-lg shadow-md w-full sm:w-3/4 md:w-2/3 lg:w-auto"
+                        alt="decorative"
+                        src="https://firebasestorage.googleapis.com/v0/b/supporttickets-cb452.appspot.com/o/ecocalor%2Fse%C3%B1ora.jpg?alt=media&token=75329952-13ec-4aaf-ad28-5cf896b604a3"
+                    />
                 </div>
+
                 <div>
-                    <div className="mt-5 ml-10">
-                        <img className='object-cover rounded-lg shadow-md instalation' alt="decorative" src="https://firebasestorage.googleapis.com/v0/b/supporttickets-cb452.appspot.com/o/ecocalor%2FinstalacionTermica.jpg?alt=media&token=ceb27a0e-c49c-4089-87f6-2f0c7f105d3b"></img>
-                    </div>
+                    <h2 className="uppercase text-red-700 text-xl lg:text-2xl">instalación térmica</h2>
+                    <p className="text-justify text-sm lg:text-base">
+                        La moderna tecnología del sistema de instalación térmica,
+                        permite obtener una calefacción en los ambientes de la edificación,
+                        utilizando la radiación del sol, que por medio de la emisión, propagación y
+                        transferencia de energia en forma de ondas electromagnéticas o partículas, que proporciona calor;
+                        el cual viene transferida al fluido vector, esta energia incrementa el calor
+                        del fluido al interno de las tuberias.
+                        El incremento de temperatura del fluido vector es transmitido a los
+                        ambientes requeridos por medio de los escambiadores de calor,
+                        utilizando la convección y radiación para transferir calor.
+                        Nuestro sistema de instalación térmica, se integra
+                        perfectamente con las envolventes de la edificación; otorgando
+                        como resultado el comfort térmico deseado, con temperaturas comprendidas 
+                        entre 20°C y 25°C con una humedad relativa entre 40% y 60%; siendo estos los
+                        parametros para un correcto ambiente comfortable, según el organismo internacional ASHRAE.
+                    </p>
                 </div>
-                {/* <div className="text-center">6</div> */}
-                
+
+                <div className="flex justify-center mt-5 lg:mt-0">
+                    {/* <img
+                        className="object-cover rounded-lg shadow-md w-full sm:w-3/4 md:w-2/3 lg:w-auto"
+                        alt="decorative"
+                        src="https://firebasestorage.googleapis.com/v0/b/supporttickets-cb452.appspot.com/o/ecocalor%2FinstalacionTermica.jpg?alt=media&token=ceb27a0e-c49c-4089-87f6-2f0c7f105d3b"
+                    /> */}
+
+                    <img className='object-cover rounded-lg shadow-md instalation' 
+                        alt="decorative" 
+                        src="https://firebasestorage.googleapis.com/v0/b/supporttickets-cb452.appspot.com/o/ecocalor%2FinstalacionTermica.jpg?alt=media&token=ceb27a0e-c49c-4089-87f6-2f0c7f105d3b"></img>
+                </div>
             </div>
-            <Footer/>
-           
+            <Footer />
         </div>
-    )
+    );
 }
